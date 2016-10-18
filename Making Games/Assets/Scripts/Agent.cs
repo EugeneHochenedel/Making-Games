a75b15011078406fd2d30b34edd3ebe9bf44a4ee
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-10-18 15:07:14 -0500

    Start on Projectile
    Began working on scripts for the projectile. Implemented a basic seeking behavior. Began expanding upon the behavior with the use of interfaces and classes. Boids!

---
+++ Making Games/Assets/Scripts/Agent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Boid;
+
+public class Agent : IBoids
+{
+    private Vector3 v_Velocity;
+    private Vector3 v_Pos;
+    private float f_Mass;
+
+    public Vector3 Velocity
+    {
+        get { return v_Velocity; }
+        set { v_Velocity = value; }
+    }
+
+    public Vector3 Position
+    {
+        get { return v_Pos; }
+        set { v_Pos = value; }
+    }
+
+    public float Mass
+    {
+        get { return f_Mass; }
+        set { f_Mass = value; }
+    }
+
+    public Agent(float fM)
+    {
+        Velocity = new Vector3();
+        Position = new Vector3();
+        Mass = (fM <= 0) ? 1 : fM;
+	}
+
+    public void velocityUpdate()
+    {
+        Position += Velocity;
+    }
+}
--- Making Games/Assets/Scripts/ProjectileBehavior.cs
+++ Making Games/Assets/Scripts/ProjectileBehavior.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ProjectileBehavior : MonoBehaviour {
+public class ProjectileBehavior : MonoBehaviour
+{
+    //public Vector3 Force;
+    //Vector3 currentVelocity;
+    //public int Mass;
+
+    public Transform targetPosition;
+    Vector3 SteeringForce;
+    Vector3 targetVelocity;
+    public float Seek;
 
 	// Use this for initialization
-	void Start () {
-	
+	void FixedUpdate ()
+    {
+        //transform.position = transform.position + (currentVelocity / Mass).normalized;
+
+        //targetVelocity = (targetPosition.position - transform.position).normalized;
+        //SteeringForce = (targetVelocity - currentVelocity).normalized / Mass;
+        //currentVelocity = currentVelocity + SteeringForce;
+
+        //if(currentVelocity.magnitude > 5)
+        //{
+        //    currentVelocity = currentVelocity.normalized;
+        //}
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void LateUpdate ()
+    {
+        //transform.position += currentVelocity;
 	}
 }

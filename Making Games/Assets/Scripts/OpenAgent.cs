6baa8de7f90886c160b44dbb6457a7bcbeecaa13
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-10-18 17:05:29 -0500

    Further Seeking
    Reimplemented basic seeking behavior
    Began creating an class that accesses private information from the Agent
    class

---
+++ Making Games/Assets/Scripts/OpenAgent.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Boid;
+
+public class OpenAgent : MonoBehaviour, IBoids
+{
+    private Agent bond;
+    public float mass;
+
+    public Vector3 Velocity
+    {
+        get { return bond.Velocity; }
+        set { bond.Velocity = value; }
+    }
+
+    public Vector3 Position
+    {
+        get { return bond.Position; }
+        set { bond.Position = value; }
+    }
+
+    float IBoids.Mass
+    {
+        get { return bond.Mass; }
+        set { bond.Mass = value; }
+    }
+
+    void Start()
+    {
+        bond = new Agent(mass);
+    }
+
+    void LateUpdate()
+    {
+        bond.velocityUpdate();
+    }
+}
+
+
--- Making Games/Assets/Scripts/ProjectileBehavior.cs
+++ Making Games/Assets/Scripts/ProjectileBehavior.cs
@@ -4,33 +4,33 @@ using System.Collections;
 public class ProjectileBehavior : MonoBehaviour
 {
     //public Vector3 Force;
-    //Vector3 currentVelocity;
-    //public int Mass;
+    Vector3 currentVelocity;
+    public float Mass;
 
     public Transform targetPosition;
     Vector3 SteeringForce;
     Vector3 targetVelocity;
-    public float Seek;
+    //public float Seek;
 
 	// Use this for initialization
 	void FixedUpdate ()
     {
         //transform.position = transform.position + (currentVelocity / Mass).normalized;
 
-        //targetVelocity = (targetPosition.position - transform.position).normalized;
-        //SteeringForce = (targetVelocity - currentVelocity).normalized / Mass;
-        //currentVelocity = currentVelocity + SteeringForce;
+        targetVelocity = (targetPosition.position - transform.position).normalized;
+        SteeringForce = (targetVelocity - currentVelocity).normalized / Mass;
+        currentVelocity += SteeringForce;
 
-        //if(currentVelocity.magnitude > 5)
-        //{
-        //    currentVelocity = currentVelocity.normalized;
-        //}
+        if(currentVelocity.magnitude > 5)
+        {
+            currentVelocity = currentVelocity.normalized;
+        }
 
 	}
 	
 	// Update is called once per frame
 	void LateUpdate ()
     {
-        //transform.position += currentVelocity;
+        transform.position += currentVelocity;
 	}
 }
